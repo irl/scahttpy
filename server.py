b5a79fc91a4621a25d8f563f774ace4163eae1a2
Iain R. Learmonth <irl@fsfe.org>
2014-11-11 04:25:00 +0000

    Storing some state now, needs refactoring

--- server.py
+++ server.py
@@ -15,10 +15,12 @@ from scapy.all import *
 # Configuration
 # ----------------------------------------------------------------------
 
-SERVER_ADDRESS="127.0.0.99" # Using the loopback interface is a bad idea
-SERVER_PORT=80              # as packets will get recieved twice since
-                            # no deduplication is performed
+SERVER_ADDRESS="192.168.1.5" # Using the loopback interface is a bad idea
+SERVER_PORT=80               # as packets will get recieved twice since
+                             # no deduplication is performed
 
+# ----------------------------------------------------------------------
+# Useful Constants
 # ----------------------------------------------------------------------
 
 FIN = 0x01
@@ -30,6 +32,19 @@ URG = 0x20
 ECE = 0x40
 CWR = 0x80
 
+# ----------------------------------------------------------------------
+# Global State
+# ----------------------------------------------------------------------
+
+conns = {}
+
+# ----------------------------------------------------------------------
+# Serious Business Logic
+# ----------------------------------------------------------------------
+
+def hash(p):
+    return "{}:{}".format(p['IP'].src, p['TCP'].sport)
+
 def build_synack(syn):
     dst = syn['IP'].src
     dport = syn['TCP'].sport
@@ -39,18 +54,30 @@ def build_synack(syn):
     tcp = TCP(sport=SERVER_PORT, dport=dport, flags="SA", seq=seq, ack=ack, options=[('MSS', 1460)])
     return ip/tcp
 
+def update_rem_ack(p):
+    conns[hash(p)]['rem_ack'] = p['TCP'].ack
+    if not 'ack' in conns[hash(p)].keys():
+        conns[hash(p)]['keys'] = p
+
 def handle_packet(p):
-    if p['TCP'].flags & SYN:
-        print "Recieved a SYN from {}".format(p['IP'].src)
-        if not p['TCP'].flags & ACK:
-            send(build_synack(p))
-    print p.summary()
+    print "Incoming: {}".format(p.summary())
+    if p['TCP'].flags & SYN and not p['TCP'].flags & ACK:
+        print "Recieved a SYN from {}, replying with SYNACK".format(p['IP'].src)
+        synack = build_synack(p)
+        conns[hash(p)] = {
+                'syn': p,
+                'synack': synack,
+                'seq': p['TCP'].seq,
+                'ack': p['TCP'].seq + 1
+                }
+        send(build_synack(p))
+    if p['TCP'].flags & ACK and not p['IP'].src == SERVER_ADDRESS:
+        print "Recieved an ACK from {}, ACK'd {}".format(p['IP'].src, p['TCP'].ack)
+        update_rem_ack(p)
 
 if __name__ == "__main__":
-    try:
-        sniff(filter="tcp and port {}".format(SERVER_PORT),
-            prn=lambda p: handle_packet(p)) # Listen for incoming packets
-    except KeyboardInterrupt:
-        print "Dying at the request of the user..."
-        print "Note: Existing connections may still complete."
+    lfilter = lambda (r): TCP in r and (r[TCP].dport == 80 or r[TCP].sport == 80)
+    sniff(lfilter=lfilter, prn=lambda p: handle_packet(p))
+    print conns
+    print "Dying at the request of the user..."
 
